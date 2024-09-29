fa12ef9a3564eb1ff9ddf386d9109eb5719e65b6
MohinurHim <himmatovamohinur@gmail.com>
2024-09-29 12:14:58 +0300

    Add files via upload
    Unpacking positional parametrs

---
+++ module_3_3.py
@@ -0,0 +1,14 @@
+#Распаковка
+# 1
+def print_params(a=1, b='string', c=True):
+    print(a,b,c)
+print_params()
+#print_params(a,b,c,d)
+print_params(a=1, b=25, c=[1, 2, 3])
+# 2
+values_list = [25, 'string']
+values_dict = {'c': True}
+print_params(*values_list, **values_dict)
+# 3
+values_list_2 = [54.32, 'string']
+print_params(*values_list_2, 42)
